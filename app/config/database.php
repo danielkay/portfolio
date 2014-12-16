aebaf375e1cca37707ea68c30025770a147e0955
Daniel Kay <contact@danielkay.co.uk>
2014-12-16 23:59:47 +0000

    referenced config environment vars

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => MYSQL_ADDON_HOST,
+			'database'  => MYSQL_ADDON_DB,
+			'username'  => MYSQL_ADDON_USER,
+			'password'  => MYSQL_ADDON_PASSWORD,
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

f03a346d0fced5004758d2d49fd5d4ef4fac3542
Daniel Kay <contact@danielkay.co.uk>
2014-12-24 01:56:13 +0000

    added Latest call to home controller

--- app/assets/js/controllers/homeCtrl.js
+++ app/assets/js/controllers/homeCtrl.js
@@ -1,4 +1,18 @@
 angular.module('homeCtrl', [])
-	.controller('homeController', function($scope, $http, $state) {
+	.controller('homeController', function($scope, $http, $state, Latest) {
 		
+		$scope.posts = {};
+		$scope.projects = {};
+		$scope.tutorials = {};
+
+		$scope.loading = true;
+
+		Latest.get()
+			.success(function(data) {
+				$scope.posts = data.posts;
+				$scope.projects = data.projects;
+				$scope.tutorials = data.tutorials;
+
+				$scope.loading = false;
+			});
 	});

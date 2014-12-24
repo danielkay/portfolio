1503633c695a7c2760091ee9e9621e340e01a4a8
Daniel Kay <contact@danielkay.co.uk>
2014-12-24 02:07:07 +0000

    added TutorialController

---
+++ app/controllers/TutorialController.php
@@ -0,0 +1,93 @@
+<?php
+
+class TutorialController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return Response::json(Tutorial::get());
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		Tutorial::create(array(
+			'title' => Input::get('title'),
+			'content' => Input::get('content')
+		));
+
+		return Response::json(['success' => true]);
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return Response::json(Tutorial::find($id));
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		Tutorial::destroy($id);
+
+		return Response::json(['success' => true]);
+	}
+
+
+}
